682fe70a53218504041ebb1c1f871c8a5b0109ee
mike belshe <mike@bitgo.com>
2014-01-13 11:30:13 -0800

    Small cleanups to signandverify test

--- test/benchmark/signandverify.js
+++ test/benchmark/signandverify.js
@@ -4,25 +4,24 @@ var SignAndVerify = new BenchmarkSuite('SignAndVerify', [69930], [
 ]);
 
 
-var private_key = new Bitcoin.ECKey();
+var privateKey = new Bitcoin.ECKey();
 var message = "this is the bitcoin transaction";
 var sigHex;
 
 function signMessage() {
   var hash = Crypto.SHA256(Crypto.SHA256(message, {asBytes: true}), {asBytes: true});
-  var sig = private_key.sign(hash);
+  var sig = privateKey.sign(hash);
   var obj = Bitcoin.ECDSA.parseSig(sig);
   sigHex = Crypto.util.bytesToHex(integerToBytes(obj.r, 32))+Crypto.util.bytesToHex(integerToBytes(obj.s, 32));
 }
 
 function verifyMessage() {
-  var adr = private_key.getBitcoinAddress();
-  var pub = private_key.getPub();
+  var adr = privateKey.getBitcoinAddress().toString();
   var sig = sigHex;
 
   var hash = Crypto.SHA256(Crypto.SHA256(message, {asBytes: true}), {asBytes: true});
 
-  var sig = [27].concat(Crypto.util.hexToBytes(sig));
+  sig = [27].concat(Crypto.util.hexToBytes(sig));
   sig = Bitcoin.ECDSA.parseSigCompact(sig);
 
   var res = false;
